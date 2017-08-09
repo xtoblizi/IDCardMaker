bd94bcf38fa3c87d030e60b7707ac6ee0176c42d
AJILEYE AYOKUNLE J <kunlesymls@hotmail.com>
2017-08-09 14:10:32 +0100

    Adding dev branch to the repo

--- IDCardMakerModel/Enrollment.cs
+++ IDCardMakerModel/Enrollment.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace IDCardMakerModel
+﻿namespace IDCardMakerModel
 {
-    class Enrollment
+    public class Enrollment
     {
     }
 }
